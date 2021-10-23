62bed1c1dbb01c0ce5a82f1589efb5e253697791
lexsander ikhaylov <mihaleksval@gmail.com>
2021-10-23 16:18:47 +0500

    again es5

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
-import express from 'express';
+var express = require('express');
 var app = express();
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
 console.log('Hello World');
 
-export default app;
+module.exports = app;
