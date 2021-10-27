dd5fab77baa9bd7f7c0a082abe19ea8a9197a567
lexsander ikhaylov <mihaleksval@gmail.com>
2021-10-27 23:38:22 +0500

    express static

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use('/', (req, res) => {
+  express.static(__dirname + '/public/style.css');
+});
 console.log('Hello World');
 
 module.exports = app;
