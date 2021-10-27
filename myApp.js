5d27408f23a1306df3f6780e1da96588749cab56
lexsander ikhaylov <mihaleksval@gmail.com>
2021-10-27 23:42:43 +0500

    again app use

--- myApp.js
+++ myApp.js
@@ -3,9 +3,8 @@ var app = express();
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-app.use('/', (req, res) => {
-  express.static(__dirname + '/public/style.css');
-});
+app.use('/public', express.static(__dirname + '/public'));
+
 console.log('Hello World');
 
 module.exports = app;
