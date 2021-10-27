c31811c7f7e7261c87b1880da8c17ac7249d42d0
lexsander ikhaylov <mihaleksval@gmail.com>
2021-10-27 21:54:37 +0500

    --dirname

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Express');
   res.sendFile(__dirname + '/views/index.html')
 });
 console.log('Hello World');
