7ec399878c0b19b19eb4e6e38c1d63dc56b9ef98
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-02 23:14:43 +0500

    hello json

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+app.get('/json', (req, res) => {
+  res.json({
+    message: 'Hello json',
+  });
 });
 app.use('/public', express.static(__dirname + '/public'));
 
 console.log('Hello World');
 
 module.exports = app;
-  
