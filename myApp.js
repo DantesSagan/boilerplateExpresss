ffd0db5c7d6446531bfc56451aa394f90f592a89
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-03 22:29:09 +0500

    env variables

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
+var dotenv = require('dotenv').config();
 var express = require('express');
 var app = express();
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/views/index.html');
 // });
+if ((process.env.MESSAGE_STYLE = 'uppercase')) {
+  response = 'Hello json'.toUpperCase();
+} else {
+  response = 'Hello json';
+}
 app.get('/json', (req, res) => {
   res.json({
     message: 'Hello json',
