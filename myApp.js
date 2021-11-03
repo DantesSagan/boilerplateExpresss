5146b56381f3503f3f22164d03bc4f05f11b11c6
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-03 23:41:46 +0500

    10th commit

--- myApp.js
+++ myApp.js
@@ -1,22 +1,21 @@
-console.log(require('dotenv').config());
 var express = require('express');
 var app = express();
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/views/index.html');
 // });
-var response = 'Hello json'.toUpperCase();
-if (process.env.MESSAGE_STYLE === 'uppercase') {
-  response = 'Hello json'.toUpperCase();
-} else {
-  response = 'HELLO JSON';
-}
+
 app.get('/json', (req, res) => {
+  var response = 'Hello json'.toUpperCase();
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    response = 'Hello json'.toUpperCase();
+  } else {
+    response = 'HELLO JSON';
+  }
   res.json({
     message: response,
   });
+  console.log(response);
 });
 // app.use('/public', express.static(__dirname + '/public'));
-
-console.log('Hello World');
 
 module.exports = app;
