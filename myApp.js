fe2e129a4da2845f48ce987add8a05725aa06ee2
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-04 12:23:28 +0500

    18th commit

--- myApp.js
+++ myApp.js
@@ -2,19 +2,18 @@ require('dotenv').config();
 const mySecret = process.env['MESSAGE_STYLE'];
 var express = require('express');
 var app = express();
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/views/index.html');
-// });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 app.get('/json', (req, res) => {
   var response = { message: 'Hello json' };
-  process.env.MESSAGE_STYLE === 'uppercase'
-    ? (response.message = response.message.toUpperCase())
-    : (response = 'HELLO JSON');
+  if (process.env.MESSAGE_STYLE === 'uppercase')
+    response.message = response.message.toUpperCase();
 
   res.json(response);
   console.log(response);
 });
-// app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
