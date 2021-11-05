357787ef677e81cd487a1320ff0b2acfb830d678
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-05 20:16:39 +0500

    13th commit on middleware and finish

--- myApp.js
+++ myApp.js
@@ -2,13 +2,10 @@ require('dotenv').config();
 const mySecret = process.env['MESSAGE_STYLE'];
 var express = require('express');
 var app = express();
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.post((req, res, next) => {
+app.use((req, res, next) => {
   // var request = req.method + ' ' + req.path + ' - ' + req.ip;
   // console.log('GET /json - ::ffff:127.0.0.1', request);
   let get = ('GET', req.method);
@@ -16,6 +13,10 @@ app.post((req, res, next) => {
   let ip = ('::ffff:127.0.0.1', req.ip);
   console.log(get + ' ' + json + ' - ' + ip);
   next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', (req, res) => {
