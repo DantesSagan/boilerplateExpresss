76f9375ef9e1021e15db64bbf5f9e60f902d38f3
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-05 21:43:48 +0500

    may be done

--- myApp.js
+++ myApp.js
@@ -32,11 +32,10 @@ app.get(
   '/now',
   (req, res, next) => {
     req.time = new Date().toString();
-
     next();
   },
   (req, res) => {
-    setTimeout({ time: req.time });
+    res.send({ time: req.time });
   }
 );
 
