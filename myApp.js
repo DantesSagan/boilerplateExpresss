9868dd071afdc75491a1ae5c18634a6b45e1d863
lexsander ikhaylov <mihaleksval@gmail.com>
2021-11-09 21:26:13 +0500

    hellojson

--- myApp.js
+++ myApp.js
@@ -39,4 +39,11 @@ app.get(
   }
 );
 
+app.get('/:hello1/:helloJson1', (req, res) => {
+  hello1 = req.params.hello1;
+  helloJson1 = req.params.helloJson1;
+  req.url = '/:hello1/:helloJson1';
+  let { hello1, helloJson1 } = req.params;
+  req.json(req.params);
+});
 module.exports = app;
